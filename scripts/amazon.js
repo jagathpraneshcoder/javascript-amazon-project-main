4fc900531f4619832bf87f19deb5f7f69c36779d
jagathpraneshrameshbabu@gmail.com <jagathpraneshrameshbabu@gmail.com>
2024-08-19 15:06:07 +0530

    Cart Value using selector tool is made

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -27,7 +27,7 @@ products.forEach(product=>{
               </div>
     
               <div class="product-quantity-container">
-                <select>
+                <select class="js-quantity-selector-${product.id}">
                   <option selected value="1">1</option>
                   <option value="2">2</option>
                   <option value="3">3</option>
@@ -59,6 +59,8 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
     button.addEventListener('click',()=>{
       const productId=button.dataset.productId;
       let matchingId;
+      const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+      const quantity = Number(quantitySelector.value);
       cart.forEach(item => {
           if(productId === item.productId){
             matchingId=item;
@@ -66,12 +68,12 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
       });
 
       if(matchingId){
-        matchingId.quantity+=1;
+        matchingId.quantity+=quantity;
       }
       else{
         cart.push({
           productId:productId,
-          quantity:1
+          quantity:quantity
         });
       }
       cartQuantity=0;
